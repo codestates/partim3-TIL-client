a309cdde4c846dfd2b1d6705957bade98ee6a29c
dasom <dasom.choi08@gmail.com>
2020-11-09 22:55:59 +0900

    [기본환경설정]/프로젝트 시작 기본 환경 설정

---
+++ TIL/src/config.js
@@ -0,0 +1,12 @@
+let REACT_APP_URL;
+if (process.env.NODE_ENV === "production") {
+  REACT_APP_URL = "http://ec2-52-78-31-183.ap-northeast-2.compute.amazonaws.com:5000";
+} else if (
+  process.env.NODE_ENV === "development" ||
+  process.env.NODE_ENV === "test"
+) {
+  REACT_APP_URL = "http://localhost:5000";
+}
+export default REACT_APP_URL;
+// import REACT_APP_URL from "../config"
+// ${REACT_APP_URL}
