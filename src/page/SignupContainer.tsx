da59f21bfd55915e7e0029dece0757e0306b745b
dasom <dasom.choi08@gmail.com>
2020-11-10 01:38:37 +0900

    [회원가입 구현]/회원가입 구현 중 아토믹 디자인 적용

---
+++ src/page/SignupContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '../components/atoms/Button'
+
+export default function SignupContainer() {
+    return (
+        <div>
+            <Button color="red"></Button>
+        </div>
+    )
+}
