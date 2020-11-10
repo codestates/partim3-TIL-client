3ac0474edb0e1afc7cf2b5c11e2ab7aa79506092
rbdkqk <rbdkqk@gmail.com>
2020-11-11 01:05:15 +0900

    [Prettier 설정 시도] 성공한 듯?

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello World!!
-    </div>
-  );
+  return <div className="App">Hello World!!</div>;
 }
 
 export default App;
