05a726581caa7e552e7c838e70666aad76b9220a
dasom <dasom.choi08@gmail.com>
2020-11-12 01:00:23 +0900

    [회원가입 소셜로그인 추가]/소셜로그인 버튼 추가

--- src/components/templetes/SignupContainer.tsx
+++ src/components/templetes/SignupContainer.tsx
@@ -1,24 +1,69 @@
 import React from 'react';
-import Button from '../UI/atoms/Button';
+import ButtonBoot from '../UI/atoms/ButtonBoot';
 import Text from '../UI/atoms/Text';
+import { Link } from 'react-router-dom';
 
 import FormBoot from '../UI/atoms/FormBoot';
 
 import { Container, Row, Col, Image } from 'react-bootstrap';
+import { BsFillAwardFill } from 'react-icons/bs';
 
 export default function SignupContainer() {
   return (
     <Container className="py-5">
       <Row>
         <Col className="m-auto" xs={10} sm={8} md={6}>
-          {/* <Button color="red"></Button>  */}
-          {/* <Text text="kkk"></Text> */}
           <Col className="m-auto pb-3">
             <Image src="img/cat.jpeg" height="171" width="180" roundedCircle />
           </Col>
-          <FormBoot type="email" placeholder="email"></FormBoot>
-          <FormBoot type="nickname" placeholder="nickname"></FormBoot>
-          <FormBoot type="password" placeholder="password"></FormBoot>
+          <Row className="m-auto">
+            <Col xs={2} sm={2} md={2}>
+              <BsFillAwardFill className="m-2"></BsFillAwardFill>
+            </Col>
+            <Col xs={10} sm={10} md={10}>
+              <FormBoot type="email" placeholder="email"></FormBoot>
+            </Col>
+          </Row>
+          <Row className="m-auto">
+            <Col xs={2} sm={2} md={2}>
+              <BsFillAwardFill className="m-2"></BsFillAwardFill>
+            </Col>
+            <Col xs={10} sm={10} md={10}>
+              <FormBoot type="nickname" placeholder="nickname"></FormBoot>
+            </Col>
+          </Row>
+          <Row className="m-auto">
+            <Col xs={2} sm={2} md={2}>
+              <BsFillAwardFill className="m-2"></BsFillAwardFill>
+            </Col>
+            <Col xs={10} sm={10} md={10}>
+              <FormBoot type="password" placeholder="password"></FormBoot>
+            </Col>
+          </Row>
+          <Row className="m-2">
+            <ButtonBoot title="Get start!" color="success"></ButtonBoot>
+          </Row>
+          <hr
+            style={{
+              backgroundColor: 'gray',
+              height: 2,
+            }}
+          />
+          <Row className="m-2">
+            <ButtonBoot title="kakao" color="warning"></ButtonBoot>
+          </Row>
+          <Row className="m-2">
+            <ButtonBoot title="naver" color="success"></ButtonBoot>
+          </Row>
+          <Row className="m-2">
+            <ButtonBoot title="google" color="danger"></ButtonBoot>
+          </Row>
+          <Row className="m-2">
+            <ButtonBoot title="github" color="dark"></ButtonBoot>
+          </Row>
+          <div className="mt-4">
+            <Link to="/login">회원이신가요?</Link>
+          </div>
         </Col>
       </Row>
     </Container>
