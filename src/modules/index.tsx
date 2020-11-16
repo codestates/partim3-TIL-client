c978b2c9eced003f074ddb40efd795116cf52b8f
rbdkqk <rbdkqk@gmail.com>
2020-11-17 00:25:37 +0900

    [redux 개선] : 새로고침에도 state 유지하기 (redux-persist)
      - 'typescript를 적용해야 하는데 이게 어렵다'고 느꼈었음
      - 천천히 다시 보니 persistConfig 안에서 whitelist 지정을 제대로 안 해 줘서 생긴 문제인 듯.
      - 실험결과 로그인이 유지되고 있음
    
      - 궁금한 점 :
          - 유저가 로그인을 하면 로그아웃을 누르기 전까지는 계속 로그인이 유지되도록 할 것인지(localStorage(?))
          - 로그아웃을 안해도 브라우저나 컴퓨터가 꺼지면 로그아웃되게 할 것인지(sesstionStorage(?))

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -2,6 +2,17 @@ import { combineReducers } from 'redux';
 import signup from './signup';
 import handleUserInfo from './handleUserInfo';
 import loginOut from './loginOut';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  // localStorage에 저장합니다.
+  storage,
+  // reducere들 중에서 이 목록에 포함된 reducer만 저장합니다.
+  whitelist: ['handleUserInfo', 'loginOut'],
+  // blacklist -> 그것만 제외합니다(여기서는 적용하지 않았음)
+};
 
 const reducers = combineReducers({
   signup,
@@ -9,6 +20,6 @@ const reducers = combineReducers({
   loginOut,
 });
 
-export default reducers;
-
 export type RootState = ReturnType<typeof reducers>;
+
+export default persistReducer(persistConfig, reducers);
