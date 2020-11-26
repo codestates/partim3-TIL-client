41d78a6557599c2d7d03141d91ca04577f1c9759
rbdkqk <rbdkqk@gmail.com>
2020-11-27 00:30:23 +0900

    [코드 정리] 쓸데없는 module(액션 및 리듀서 : getTodosList) 삭제
      - 실제로는 다른데서는 쓰이지 않고 있어서 modules 폴더 안에서만 정리했음

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import signup from './signup';
 import handleUserInfo from './handleUserInfo';
 import loginOut from './loginOut';
-import getTodosList from './getTodosList';
 
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -15,7 +14,7 @@ const persistConfig = {
   // reducere들 중에서 이 목록에 포함된 reducer만 저장합니다.
   // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
 
-  whitelist: ['handleUserInfo', 'loginOut', 'calendarDay', 'getTodosList'],
+  whitelist: ['handleUserInfo', 'loginOut', 'calendarDay'],
 
   // blacklist -> 그것만 제외합니다(여기서는 적용하지 않았음)
 };
@@ -25,7 +24,6 @@ const reducers = combineReducers({
   handleUserInfo,
   loginOut,
   calendarDay,
-  getTodosList,
 });
 
 export type RootState = ReturnType<typeof reducers>;
