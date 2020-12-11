0db91054d3fe0c47d60bbf473a1e8442186d85a1
rbdkqk <rbdkqk@gmail.com>
2020-12-11 16:26:23 +0900

    [수정] 로그인 페이지 - 부트스트랩 코드를 모두 삭제함
      - styled component로 변경함
      - 아직 엉성해서 다시 봐야 함

--- src/componentsNew/pages/Login.tsx
+++ src/componentsNew/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import styled from 'styled-components';
 
 import LoginGeneral from '../oraganisms/LoginGeneral';
 import LoginSocial from '../oraganisms/LoginSocial';
@@ -14,31 +14,60 @@ interface LoginProps {
 
 export default function Login({ handleChange, postLoginReq }: LoginProps) {
   return (
-    <Container className="py-5">
-      <Row>
-        <Col className="m-auto" xs={10} sm={8} md={6}>
+    <LoginPage>
+      <LoginWrap>
+        <div style={{ margin: '10px 0px' }}>
           {/* 고양이 이미지 */}
-          <Col className="m-auto pb-3">
-            <Link to="/">
-              <Image src="img/cat.jpeg" height="171" width="180" roundedCircle />
-            </Link>
-          </Col>
+          <Link to="/">
+            <img src="img/cat.jpeg" height="171" width="180" style={{ borderRadius: '50%' }} />
+          </Link>
+        </div>
+
+        <div style={{ margin: '10px 0px' }}>
           {/* 일반 로그인 */}
           <LoginGeneral handleChange={handleChange} postLoginReq={postLoginReq} />
+        </div>
+
+        <div>
           {/* 가로선 */}
-          <hr
-            style={{
-              backgroundColor: 'gray',
-              height: 2,
-            }}
-          />
+          <hr style={{ height: 3, width: '300px', background: 'gray' }}></hr>
+        </div>
+
+        <div style={{ margin: '10px 0px', width: '100%' }}>
           {/* 소셜 로그인 : 네이버, 구글 */}
           <LoginSocial />
-          <div className="mt-4">
-            <Link to="/signup">아직 계정이 없으신가요?</Link>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+
+        <div
+          style={{
+            margin: '10px 0px',
+            width: '100%',
+            flexDirection: 'column',
+            display: 'flex',
+            alignItems: 'flex-end',
+          }}
+        >
+          <Link to="/signup">아직 계정이 없으신가요?</Link>
+        </div>
+      </LoginWrap>
+    </LoginPage>
   );
 }
+
+const LoginPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const LoginWrap = styled.div`
+  flex: 1;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
