4ddd1a8f5574e595434d6e118d0ccd07bef9fdad
rbdkqk <rbdkqk@gmail.com>
2020-12-26 03:59:06 +0900

    [수정] LoginContainer / SignupContainer : alert 삭제
      - ModalAlert을 최대한 재사용 가능하도록 수정했음

--- src/container/SignupContainer.tsx
+++ src/container/SignupContainer.tsx
@@ -17,9 +17,8 @@ export default function SignupContainer() {
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
   const [handleWelcomeModalAlert, setHandleWelcomeModalAlert] = useState(false);
-  const [handleErrorModalAlert, setHandleErrorModalAlert] = useState(false);
-  const [handlePostErrorModalAlert, setHandlePostErrorModalAlert] = useState(false);
-  const [postError, setPostError] = useState('');
+  const [handleModalAlert, setHandleModalAlert] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
   const { currentUser } = useSelector((state: RootState) => state.loginOut.status);
 
   const dispatch = useDispatch();
@@ -37,7 +36,8 @@ export default function SignupContainer() {
 
   if (currentUser) {
     // alert('로그인한 상태에서는 회원가입을 할 수 없습니다. 로그아웃 후 시도하세요'); 를 대신함
-    setHandleSendToCalDayModalAlert(true);
+    setModalMessage('로그인한 상태에서는 회원가입을 할 수 없습니다. 로그아웃 후 시도하세요.');
+    setHandleModalAlert(true);
   }
 
   const handleChange = (
@@ -86,7 +86,10 @@ export default function SignupContainer() {
             { withCredentials: true },
           )
           .then(res => {
-            setHandleWelcomeModalAlert(true); // alert('환영합니다'); 를 대신함
+            // setModalMessage('환영합니다');
+            // setHandleModalAlert(true);
+            // alert('환영합니다'); 를 대신함
+            setHandleWelcomeModalAlert(true);
           })
           .catch(err => {
             console.log(err);
@@ -94,7 +97,8 @@ export default function SignupContainer() {
       })
       .catch(err => {
         // alert(`이미 가입된 이메일 또는 닉네임입니다.`); 를 대신함
-        setHandleErrorModalAlert(true);
+        setModalMessage(`이미 가입된 이메일 또는 닉네임입니다.`);
+        setHandleModalAlert(true);
         dispatch(signupFailure());
       });
   };
@@ -102,18 +106,18 @@ export default function SignupContainer() {
   const postSignupReq = () => {
     if (email === '') {
       // alert('이메일을 입력하지 않으셨습니다. 이메일을 입력해 주세요.');
-      setPostError('이메일을 입력하지 않으셨습니다. 이메일을 입력해 주세요.');
-      setHandlePostErrorModalAlert(true);
+      setModalMessage('이메일을 입력하지 않으셨습니다. 이메일을 입력해 주세요.');
+      setHandleModalAlert(true);
       return;
     } else if (password === '') {
       // alert('비밀번호를 입력하지 않으셨습니다. 비밀번호를 입력해 주세요.');
-      setPostError('비밀번호를 입력하지 않으셨습니다. 비밀번호를 입력해 주세요.');
-      setHandlePostErrorModalAlert(true);
+      setModalMessage('비밀번호를 입력하지 않으셨습니다. 비밀번호를 입력해 주세요.');
+      setHandleModalAlert(true);
       return;
     } else if (password !== passwordConfirm) {
       // alert('입력하신 비밀번호가 서로 다릅니다. 동일한 비밀번호를 입력해 주세요.');
-      setPostError('입력하신 비밀번호가 서로 다릅니다. 동일한 비밀번호를 입력해 주세요.');
-      setHandlePostErrorModalAlert(true);
+      setModalMessage('입력하신 비밀번호가 서로 다릅니다. 동일한 비밀번호를 입력해 주세요.');
+      setHandleModalAlert(true);
       return;
     } else {
       let passwordValidation = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/;
@@ -121,10 +125,10 @@ export default function SignupContainer() {
         // alert(
         //   '비밀번호가 조건에 맞지 않습니다. 8~12글자 사이의 영문 대소문자와 숫자의 조합으로 구성하세요.',
         // );
-        setPostError(
+        setModalMessage(
           '비밀번호가 조건에 맞지 않습니다. 8~12글자 사이의 영문 대소문자와 숫자의 조합으로 구성하세요.',
         );
-        setHandlePostErrorModalAlert(true);
+        setHandleModalAlert(true);
         return;
       } else {
         handleRegister(email, nickname, password);
@@ -138,21 +142,12 @@ export default function SignupContainer() {
     history.push('/login');
   };
 
-  const handleCloseErrorModal = () => {
-    setHandleErrorModalAlert(false);
-  };
-
-  const handleClosePostErrorModal = () => {
-    setHandlePostErrorModalAlert(false);
+  const handleCloseModal = () => {
+    setHandleModalAlert(false);
   };
 
   return (
     <>
-      <ModalAlert
-        title="로그인한 상태에서는 회원가입을 할 수 없습니다. 로그아웃 후 시도하세요!"
-        isVisible={handleSendToCalDayModalAlert}
-        handleCloseModal={handleCloseSendToCalDayModal}
-      />
       <Signup handleChange={handleChange} postSignupReq={postSignupReq} />
       <ModalAlert
         title="환영합니다!"
@@ -160,14 +155,9 @@ export default function SignupContainer() {
         handleCloseModal={handleCloseWelcomeModal}
       />
       <ModalAlert
-        title="이미 가입된 이메일 또는 닉네임입니다."
-        isVisible={handleErrorModalAlert}
-        handleCloseModal={handleCloseErrorModal}
-      />
-      <ModalAlert
-        title={`${postError}`}
-        isVisible={handlePostErrorModalAlert}
-        handleCloseModal={handleClosePostErrorModal}
+        title={`${modalMessage}`}
+        isVisible={handleModalAlert}
+        handleCloseModal={handleCloseModal}
       />
     </>
   );
